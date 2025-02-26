892ba2652785d0de464fca02f7ce40abbfa8e613
Tehoor Marjan <tehoor.marjan@gmail.com>
2025-02-27 00:05:56 +0100

    Implement GPL formatting logic in GplFormatter for palette export

--- src/formatters/gpl.rs
+++ src/formatters/gpl.rs
@@ -1,3 +1,5 @@
+use palette::Srgb;
+
 use crate::palette::Palette;
 use crate::formatters::Formatter;
 
@@ -5,7 +7,16 @@ pub struct GplFormatter;
 
 impl Formatter for GplFormatter {
     fn format_palette(&self, palette: &Palette) -> Result<String, Box<dyn std::error::Error>> {
-        // Implement the logic to format the palette as a GPL string
-        Ok(String::new())
+        let mut gpl_string = String::new();
+        gpl_string.push_str("GIMP Palette\n");
+        gpl_string.push_str(&format!("Name: {}\n", palette.name));
+        gpl_string.push_str("#\n");
+
+        for swatch in &palette.swatches {
+            let (r, g, b) = Srgb::<u8>::from_format(swatch.color).into_components();
+            gpl_string.push_str(&format!("{:3} {:3} {:3} {}\n", r, g, b, swatch.name));
+        }
+
+        Ok(gpl_string)
     }
 }
