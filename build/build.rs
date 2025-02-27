1223c86e75a76862c91f825cff5aafd3cd5267a3
Tehoor Marjan <tehoor.marjan@gmail.com>
2025-02-27 01:16:28 +0100

    Add .env.default file and update dotenv dependency to dotenvy

--- build/build.rs
+++ build/build.rs
@@ -1,10 +1,15 @@
-use dotenv::dotenv;
+use dotenvy::dotenv;
+use dotenvy::from_path;
 use std::env;
 use std::fs::{self, File};
 use std::io::Write;
 
 fn main() {
+    // Load .env file if present
     dotenv().ok();
+    // Load default values from .env.default
+    from_path(".env.default").ok();
+
     let config_url = env::var("CONFIG_URL").expect("CONFIG_URL must be set");
 
     // Create the gen directory if it does not exist
